2273dce6008b6a1736aeec39daa89300649570cf
kemalaperenkucuktag <kemalalperenkucuktag@gmail.com>
2024-08-17 12:24:29 +0300

    new update

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,11 @@
 const http = require("http");
+
+//1-Server created
+const server = http.createServer((req, res) => {});
+
+//2-belirli porta gelen istekleri dinle
+const port = 4006;
+
+server.listen(port, () => {
+  console.log();
+});
